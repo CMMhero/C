b0cb9bd313ecf6fd23fcb000ff081affb07b27ca
CMMhero <ch.mh.marvelio@gmail.com>
2021-04-27 20:44:10 +0700

    Week 11

--- Algorithm and Data Structure - LAB/Week 1/lowercase.c
+++ Algorithm and Data Structure - LAB/Week 1/lowercase.c
@@ -5,15 +5,14 @@ int main()
 	printf("\n");
 	printf("Masukkan kata: ");
 	scanf("%[^\n]", &kalimat);
-	
+
 	int i;
-	for(i=0; kalimat[i]!='\0'; i++)
+	for (i = 0; kalimat[i] != '\0'; i++)
 	{
-		if(kalimat[i]>='A' && kalimat[i]<='Z')
-			kalimat[i]+=32;
+		if (kalimat[i] >= 'A' && kalimat[i] <= 'Z')
+			kalimat[i] += 32;
 	}
 	printf("%s", kalimat);
-	
+
 	return 0;
 }
-
